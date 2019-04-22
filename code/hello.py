ae666fb76278fe32f90e25fd679799124fce6b9f
QRobot <qiuwenhui1288@qq.com>
2019-04-22 12:32:36 +0800

    helloworld

---
+++ code/hello.py
@@ -0,0 +1,9 @@
+'''
+ # @ Author: qiuwenhui
+ # @ Create Time: 2019-04-22 12:31:38
+ # @ Modified by: qiuwenhui
+ # @ Modified time: 2019-04-22 12:31:40
+ # @ 代码描述: helloworld
+ '''
+
+print("helloworld")
